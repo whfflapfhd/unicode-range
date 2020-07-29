899942cdfa0994e5e7f8a5f3bc929bbdd2b18285
whfflapfhd <whfflapfhd@naver.com>
2020-07-29 16:53:13 +0900

    update

--- assets/js/common.js
+++ assets/js/common.js
@@ -43,6 +43,7 @@
             patten = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"],
             html = "",
             tdNode = "",
+            btn = "<button type='button' class='single-select'>+</button><button type='button' class='multi-select'>~</button>",
             range = false;
             tbl.empty();
             for(var b=0; b<patten.length; b++){
